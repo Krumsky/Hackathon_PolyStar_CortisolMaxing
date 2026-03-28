f8bcd643ffbb261653e0d182ae52aa1034ffa132
jcgauthier <jcgeau@gmail.com>
2026-03-28 18:54:51 -0400

    stepper test

--- Code/src/main.cpp
+++ Code/src/main.cpp
@@ -1,18 +1,42 @@
 #include <Arduino.h>
-#include <Dht_nonblocking.h>
+#include <Stepper.h>
+
+#include "constants.h"
+#include "step_control.h"
+// initialize the stepper library on pins 8 through 11:
+Stepper myStepper1(STEPS_PER_REVOLUTION, STEPPER1_IN4, STEPPER1_IN2,
+                   STEPPER1_IN3, STEPPER1_IN1);
+
+Stepper myStepper2(STEPS_PER_REVOLUTION, STEPPER2_IN4, STEPPER2_IN2,
+                   STEPPER2_IN3, STEPPER2_IN1);
+
+// void ISR1() { Serial.println("mouvement detected"); }
 
 void setup() {
   // put your setup code here, to run once:
+  pinMode(MOTION_SENSOR_PIN, INPUT);
+  // attachInterrupt(digitalPinToInterrupt(MOTION_SENSOR_PIN), ISR1, RISING);
+  stepControlInit(myStepper1, REVOLUTIONS_PER_MINUTE);
+  stepControlInit(myStepper2, STEERING_SPEED);
   Serial.begin(115200);
   Serial.println("Hello, World!");
   pinMode(LED_BUILTIN, OUTPUT);
+  Serial.print("entering main");
 }
 
 void loop() {
-  
-  // put your main code here, to run repeatedly:
-  digitalWrite(LED_BUILTIN, HIGH);   // turn the LED on (HIGH is the voltage level)
-  delay(1000);                       // wait for a second
-  digitalWrite(LED_BUILTIN, LOW);    // turn the LED off by making the voltage LOW
-  delay(1000);                       // wait for a second
+  Serial.println("in main");
+  delay(500);
+
+  // stepClockwise(myStepper2, STEPS_PER_REVOLUTION);
+  // delay(500);
+
+  // stepClockwise(myStepper2, STEPS_PER_REVOLUTION);
+  // delay(500);
+
+  stepClockwise(myStepper1, 500);
+  delay(500);
+
+  stepClockwise(myStepper1, -500);
+  delay(500);
 }
